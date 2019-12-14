dee7bce1032b6165b0d37e9f6bffa13b4d4dfd54
Danielle Burrage <burragedanielle@gmail.com>
2019-12-14 12:09:00 -0600

    added es6 to lint and materialize js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,55 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    // Get all examples
+    app.get("/api/examples", function(req, res) {
+        db.Example.findAll({}).then(function(dbExamples) {
+            res.json(dbExamples);
+        });
     });
-  });
+    app.get("/api/allEmployees", function(req, res) {
+        db.dbEmployees.findAll({}).then(function(dbEmployees) {
+            res.json(dbEmployees);
+        });
+    });
+    app.get("/api/allEmployees/:id", function(req, res) {
+        db.dbEmployees.findAll({}).then(function(dbEmployees) {
+            res.json(dbEmployees);
+        });
+    });
+    app.get("/api/allRequests", function(req, res) {
+        db.dbRequests.findAll({}).then(function(dbRequests) {
+            res.json(dbRequests);
+        });
+    });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    // Create a new example
+    app.post("/api/examples", function(req, res) {
+        db.Example.create(req.body).then(function(dbExample) {
+            res.json(dbExample);
+        });
     });
-  });
+    app.post("/api/newStore", function(req, res) {
+        db.newStore.create(req.body).then(function(dbnewStore) {
+            res.json(dbnewStore);
+        });
+    });
+    app.post("/api/newLocation", function(req, res) {
+        db.newLocation.create(req.body).then(function(dbnewLocation) {
+            res.json(dbnewLocation);
+        });
+    });
+    app.post("/api/newEmployee", function(req, res) {
+        db.newEmployee.create(req.body).then(function(dbnewEmployee) {
+            res.json(dbnewEmployee);
+        });
+    });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+
+    // Delete an example by id
+    app.delete("/api/examples/:id", function(req, res) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+            res.json(dbExample);
+        });
     });
-  });
 };
