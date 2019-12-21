c3c57b80acf26c64fb4e5419e1bb1498eb1d6bda
Danielle Burrage <burragedanielle@gmail.com>
2019-12-20 22:39:22 -0600

    partial functionality. not fully resolved but hoping it is close

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const fetch = require("isomorphic-unfetch");
 
 module.exports = app => {
   // Load index page
@@ -18,42 +19,44 @@ module.exports = app => {
     });
   });
 
-  const locations = [
-    {
-      locationNum: 10,
-      companyNum: 12,
-      address: "pickles",
-      city: "pickles",
-      state: "pickles",
-      openingTime: "pickles",
-      closingTime: "pickles"
-    },
-    {
-      locationNum: 11,
-      companyNum: 13,
-      address: "test",
-      city: "test",
-      state: "test",
-      openingTime: "test",
-      closingTime: "test"
-    }
-  ];
+  // REAL WORLD
+  // LogIn --> User Info (location#) --> Locations# --> Company # --> .get(/api/allLocations/:companyNum)
 
-  const days = [
-    {
-      monday: 10,
-      tuesday: 12,
-      wednesday: "pickles",
-      thursday: "pickles",
-      friday: "pickles",
-      saturday: "pickles",
-      sunday: "pickles"
-    }
-  ];
+  // TESTING
+  // enter url w/ companyName --> company # --> .get(/api/allLocations)
 
-  app.get("/manager-view", (req, res) => {
-    res.render("managerView", { locations, days });
+  app.get("/test/:name", (req, res) => {
+    const url = `http://localhost:3000/api/company/find/${req.params.name}`;
+    fetch(url)
+      .then(r => r.json())
+      .then(data => {
+        const userCompanyId = data.id;
+        const url2 = `http://localhost:3000/api/allLocations/${userCompanyId}`;
+        fetch(url2)
+          .then(res2 => res2.json())
+          .then(locations => {
+            console.log(locations);
+            res.render("managerView", { locations });
+          });
+      });
   });
+
+    // $.get("/api/company/find/" + req.params.name, data => {
+    //   res.json(data);
+    // });
+
+  // app.get("/testing/:name", (req, res) => {
+  //   console.log(req.params.companyNum);
+  //   $.get("/api/company/find/" + req.params.company-name, (data) => {
+  //     const userCompanyId = data.id;
+  //     console.log(userCompanyId); 
+  //   }).then(() => {
+  //         const userCompanyId = data.id;
+  //         $.get("/api/allLocations/" + userCompanyId, (data) => {
+  //           res.render("mangerView", {data})
+  //         });
+  //       }
+  //     );
 
   // Load example page and pass in an example by id
   app.get("/example/:id", (req, res) => {
